02a72ebbcce2b38353007857ded71cf561b6edc6
Wayzh <wayziwa@163.com>
2016-01-13 10:56:56 +0800

    test

--- javascripts/category.js
+++ javascripts/category.js
@@ -3,16 +3,18 @@ $(document).ready(function(){
 	var category = window.location.href.split("?")[1].split("=")[1];
 	$("#category").text(category);
 	$.ajax({
-		type:"post",
+		type:"get",
 		url:"posts.json",
-		 dataType: "json",
+		dataType: "json",
 		success:function(data){
 			console.log(data);
-		
+			var posts = [];
+			for()
 		},
 		error:function(a){
 			console.log("false");
-			console.log(a);
+
 		}
 	});
+	
 });
