06dca74928358f157a929e1b97f668c435770e47
wayzh <wayziwa@163.com>
2016-01-14 22:39:17 +0800

    修正头

--- js/blog.js
+++ js/blog.js
@@ -5,4 +5,9 @@ $(function(){
 	 		 "http://wayzh.github.io/img/tm-bg-slide-3.jpg"
 	 			], 	{duration: 3200, fade: 1300});
 		});
+	jQuery(document).ready(function() {
+		$('.panel-min-cover').backstretch([
+			"http://wayzh.github.io/img/tm-bg-slide-3.jpg"
+		], 	{duration: 3200, fade: 1300});
+	});
 });
