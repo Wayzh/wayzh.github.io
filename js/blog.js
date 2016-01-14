5fc85b040bdc9f5124a6314443f69583798c1cbc
Wayzh <wayziwa@163.com>
2016-01-14 17:01:58 +0800

    自定义样式，不用github上的模板

---
+++ js/blog.js
@@ -0,0 +1,8 @@
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('.panel-cover').backstretch([
+	 		 "img/tm-bg-slide-3.jpg"
+	 			], 	{duration: 3200, fade: 1300});
+		});
+});
