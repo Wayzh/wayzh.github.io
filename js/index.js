2b9ea4ac5da446704687a75100da180e1b27ecc2
Wayzh <wayziwa@163.com>
2016-01-15 11:46:54 +0800

    test
    Signed-off-by: Wayzh <wayziwa@163.com>

--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ $(window).load(function(){
 
 // HOME BACKGROUND SLIDESHOW
 $(function(){
-    jQuery(document).ready(function() {
-		$('body').backstretch([
-	 		 "http://wayzh.github.io/img/tm-bg-slide-1.jpg"
-	 			], 	{duration: 3200, fade: 1300});
-		});
+//  jQuery(document).ready(function() {
+//		$('body').backstretch([
+//	 		 "http://wayzh.github.io/img/tm-bg-slide-4.jpg"
+//	 			], 	{duration: 3200, fade: 1300});
+//		});
+		$("body").css("background-image","url(../img/tm-bg-slide-4.jpg)");
 });
