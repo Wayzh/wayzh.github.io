6704176a19c878c8b6d0cf076dabb76e54636303
Wayzh <wayziwa@163.com>
2016-01-15 11:19:01 +0800

    测试

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,20 @@
 // HOME BACKGROUND SLIDESHOW
 $(function(){
     jQuery(document).ready(function() {
-		$('.panel-cover').backstretch([
-	 		 "http://wayzh.github.io/img/tm-bg-slide-3.jpg"
-	 			], 	{duration: 3200, fade: 1300});
-		});
-	jQuery(document).ready(function() {
-		$('.panel-min-cover').backstretch([
-			"http://wayzh.github.io/img/tm-bg-slide-3.jpg"
-		], 	{duration: 3200, fade: 1300});
-	});
+//		$('.panel-cover').backstretch([
+//	 		 "http://wayzh.github.io/img/tm-bg-slide-3.jpg"
+//	 			], 	{duration: 3200, fade: 500});
+//		$('.panel-min-cover').backstretch([
+//				"http://wayzh.github.io/img/tm-bg-slide-3.jpg"
+//		], 	{duration: 3200, fade: 1300});
+	
+    	$(".panel-min-nav").hide();
+    	$("#min-nav").click(function(){
+    		$(".panel-min-nav").slideToggle(400);
+    	});
+    	
+    });
+	
+
+	
 });
