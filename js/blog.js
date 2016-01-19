8de4534251fb56591b4863025ce981b0bd4ebf10
Wayzh <wayziwa@163.com>
2016-01-19 17:09:09 +0800

    修正导航闪西现情况

--- js/blog.js
+++ js/blog.js
@@ -9,7 +9,6 @@ $(function(){
 //				"http://wayzh.github.io/img/tm-bg-slide-3.jpg"
 //		], 	{duration: 3200, fade: 1300});
 	
-    	$(".panel-min-nav").hide();
     	$("#min-nav").click(function(){
     		$(".panel-min-nav").slideToggle(400);
     	});
